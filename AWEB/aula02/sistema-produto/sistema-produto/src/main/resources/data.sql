92ba0a5c54576eac942f9c43c4f2579f8fc22ed8
cortezinho <gabrielcortez2802@gmail.com>
2025-08-13 22:23:25 -0300

    sistema-produto

--- AWEB/aula02/sistema-produto/sistema-produto/src/main/resources/data.sql
+++ AWEB/aula02/sistema-produto/sistema-produto/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO product (name, price, description) VALUES 
-('Notebook', 3500.0, 'Notebook 16GB RAM, SSD 512GB')
+('Notebook', 3500.0, 'Notebook 16GB RAM, SSD 512GB'),
 ('Smartphone', 1999.90, 'Tela 6.5", 128GB'),
 ('Table', 1200.0, 'Tela 10", 64GB'),
 ('Monitor', 899.0, 'Monitor 24" Full HD 144Hz'),
