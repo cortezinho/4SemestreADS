b63a5d5d9d57057f0d4c3708a8fe700f8958c9fa
cortezinho <gabrielcortez2802@gmail.com>
2025-08-13 22:47:53 -0300

    sistema-produto

--- AWEB/aula02/sistema-produto/sistema-produto/src/main/java/br/com/AWEB/sistema_produto/controller/ProductController.java
+++ AWEB/aula02/sistema-produto/sistema-produto/src/main/java/br/com/AWEB/sistema_produto/controller/ProductController.java
@@ -3,9 +3,12 @@ package br.com.AWEB.sistema_produto.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.AWEB.sistema_produto.model.Product;
 import br.com.AWEB.sistema_produto.service.ProductService;
+
 
 @Controller
 @RequestMapping("/products")
@@ -19,5 +22,13 @@ public class ProductController {
         model.addAttribute("products",productService.listAll());
         return "product/list";
     }
+
+    // Retorna a view do formulario de cadastro/edição de produto
+    @GetMapping("/new")
+    public String showForm(Model model) {
+        model.addAttribute("product", new Product());
+        return "product/form";
+    }
+    
 
 }
