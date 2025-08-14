0de7d0a8762dff1554abcb5377e94cebb2caf8f2
cortezinho <gabrielcortez2802@gmail.com>
2025-08-13 22:43:14 -0300

    sistema-produto

---
+++ AWEB/aula02/sistema-produto/sistema-produto/src/main/java/br/com/AWEB/sistema_produto/controller/ProductController.java
@@ -0,0 +1,23 @@
+package br.com.AWEB.sistema_produto.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.AWEB.sistema_produto.service.ProductService;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+    
+    @Autowired
+    private ProductService productService;
+
+    // Listar produtos
+    public String list(Model model){
+        model.addAttribute("products",productService.listAll());
+        return "product/list";
+    }
+
+}
