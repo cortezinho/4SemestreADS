5687b2c7b20baa8627d19c675cf3a45528fc9fda
cortezinho <gabrielcortez2802@gmail.com>
2025-08-27 22:39:37 -0300

    aula 04

--- AWEB/aula04/to-do-list/src/main/java/br/com/aweb/to_do_list/controller/TodoController.java
+++ AWEB/aula04/to-do-list/src/main/java/br/com/aweb/to_do_list/controller/TodoController.java
@@ -2,14 +2,20 @@ package br.com.aweb.to_do_list.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.aweb.to_do_list.repository.TodoRepository;
+
 @Controller
 @RequestMapping("/todo")
 public class TodoController {
+
+    @Autowired
+    TodoRepository todoRepository;
 
     @GetMapping("/home")
     public ModelAndView home() {
@@ -21,6 +27,14 @@ public class TodoController {
                 "Marie Curie",
                 "Nikola Tesla");
         modelAndView.addObject("alunos", alunos);
+        modelAndView.addObject("ehVerdade", true);
+        return modelAndView;
+    }
+
+    @GetMapping
+    public ModelAndView list() {
+        var modelAndView = new ModelAndView("list");
+        modelAndView.addObject("todos", todoRepository.findAll());
         return modelAndView;
     }
 
