8dbf7dd15b0286ee6478d39655d6db799d6aa710
cortezinho <gabrielcortez2802@gmail.com>
2025-10-08 20:38:00 -0300

    pesquisa-satisfacao

---
+++ AWEB/pesquisa-satisfacao/src/main/java/br/com/aweb/pesquisa_satisfacao/security/ConfiguracoesDeSeguranca.java
@@ -0,0 +1,23 @@
+package br.com.aweb.pesquisa_satisfacao.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class ConfiguracoesDeSeguranca {
+
+    @Bean
+    public UserDetailsService dadosUsuario(){
+        UserDetails usuario1 = User.builder().username("gabriel@email.com").password("gabriel123").build();
+        UserDetails usuario2 = User.builder().username("cortez@email.com").password("cortez123").build();
+
+        return new InMemoryUserDetailsManager(usuario1, usuario2);
+    }  
+    
+}
