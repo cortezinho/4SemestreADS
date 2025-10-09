1b4f83504705d0fa637b105f203ff179f52f1369
cortezinho <gabrielcortez2802@gmail.com>
2025-10-08 22:49:19 -0300

    pesquisa-satisfacao

---
+++ AWEB/pesquisa-satisfacao/src/main/java/br/com/aweb/pesquisa_satisfacao/model/Usuario.java
@@ -0,0 +1,13 @@
+package br.com.aweb.pesquisa_satisfacao.model;
+
+public class Usuario {
+
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private String senha;
+
+}
