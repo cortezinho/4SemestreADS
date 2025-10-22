1a07e59b571c88a199397ff571c0c0688e477873
cortezinho <gabrielcortez2802@gmail.com>
2025-10-22 18:53:57 -0300

    pesquisa-satisfacao

---
+++ AWEB/pesquisa-satisfacao-no-users/src/main/java/br/com/aweb/pesquisa_satisfacao/model/Usuario.java
@@ -0,0 +1,57 @@
+package br.com.aweb.pesquisa_satisfacao.model;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String nome;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String email;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String senha;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+    
+}
---
+++ AWEB/pesquisa-satisfacao-no-users/src/main/java/br/com/aweb/pesquisa_satisfacao/repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package br.com.aweb.pesquisa_satisfacao.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository {
+    
+}
