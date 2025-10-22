c9e26dfff603b3d20d00ce0f68d95cc704d08c6d
cortezinho <gabrielcortez2802@gmail.com>
2025-10-22 19:50:02 -0300

    pesquisa-satisfacao

--- AWEB/pesquisa-satisfacao-no-users/src/main/java/br/com/aweb/pesquisa_satisfacao/security/ConfiguracoesDeSeguranca.java
+++ AWEB/pesquisa-satisfacao-no-users/src/main/java/br/com/aweb/pesquisa_satisfacao/security/ConfiguracoesDeSeguranca.java
@@ -4,45 +4,48 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class ConfiguracoesDeSeguranca {
 
-    @Bean
-    public UserDetailsService dadosUsuarios() {
-        UserDetails usuario1 = User.builder()
-                .username("andre@email.com")
-                .password("{noop}andre123")
-                .build();
+        // @Bean
+        // public UserDetailsService dadosUsuarios() {
+        // UserDetails usuario1 = User.builder()
+        // .username("andre@email.com")
+        // .password("{noop}andre123")
+        // .build();
 
-        UserDetails usuario2 = User.builder()
-                .username("roberto@email.com")
-                .password("{noop}roberto123")
-                .build();
+        // UserDetails usuario2 = User.builder()
+        // .username("roberto@email.com")
+        // .password("{noop}roberto123")
+        // .build();
 
-        return new InMemoryUserDetailsManager(usuario1, usuario2);
-    }
+        // return new InMemoryUserDetailsManager(usuario1, usuario2);
+        // }
 
-    @Bean
-    public SecurityFilterChain filtrosSeguranca(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests(req -> {
-                    req.requestMatchers("/login", "/css/**", "/js/**", "/img/**").permitAll();
-                    req.anyRequest().authenticated();
-                })
-                .formLogin(form -> form.loginPage("/login")
-                        .defaultSuccessUrl("/")
-                        .permitAll())
-                .logout(logout -> logout
-                        .logoutSuccessUrl("/login?logout")
-                        .permitAll())
-                .rememberMe(rememberMe -> rememberMe.key("lembrarDeMim"))
-                .build();
-    }
+        @Bean
+        public SecurityFilterChain filtrosSeguranca(HttpSecurity http) throws Exception {
+                return http
+                                .authorizeHttpRequests(req -> {
+                                        req.requestMatchers("/login", "/css/**", "/js/**", "/img/**").permitAll();
+                                        req.anyRequest().authenticated();
+                                })
+                                .formLogin(form -> form.loginPage("/login")
+                                                .defaultSuccessUrl("/")
+                                                .permitAll())
+                                .logout(logout -> logout
+                                                .logoutSuccessUrl("/login?logout")
+                                                .permitAll())
+                                .rememberMe(rememberMe -> rememberMe.key("lembrarDeMim"))
+                                .build();
+        }
+
+        @Bean
+        public PasswordEncoder codificadorSenha() {
+                return new BCryptPasswordEncoder();
+        }
 }
