0218429d2cdf7e35bb2810c5fe12d36a41179897
cortezinho <gabrielcortez2802@gmail.com>
2025-11-05 22:27:56 -0300

    medpro

--- AWEB/medpro/src/main/java/com/medpro/medpro/model/dto/DadosCadastroMedico.java
+++ AWEB/medpro/src/main/java/com/medpro/medpro/model/dto/DadosCadastroMedico.java
@@ -2,6 +2,29 @@ package com.medpro.medpro.model.dto;
 
 import com.medpro.medpro.enums.Especialidade;
 
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record DadosCadastroMedico(
+
+    @NotBlank
+    String nome, 
+
+    @NotBlank
+    String email,
+    
+    @NotBlank
+    @Pattern(regexp = "\\d{4,6}")
+    String crm, 
+
+    @NotNull
+    Especialidade especialidade,
+    
+    @NotNull
+    @Valid
+    DadosEndereco endereco
+    ) {
     
 }
