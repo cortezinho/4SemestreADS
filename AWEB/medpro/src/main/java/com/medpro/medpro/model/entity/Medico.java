2cba236467cde5002e192d40c0c590e5c7ee1d37
cortezinho <gabrielcortez2802@gmail.com>
2025-11-05 22:48:05 -0300

    medpro

--- AWEB/medpro/src/main/java/com/medpro/medpro/model/entity/Medico.java
+++ AWEB/medpro/src/main/java/com/medpro/medpro/model/entity/Medico.java
@@ -33,6 +33,8 @@ public class Medico {
 
     private String email;
 
+    private String telefone;
+
     private String crm;
 
     @Enumerated(EnumType.STRING)
@@ -44,6 +46,7 @@ public class Medico {
     public Medico(DadosCadastroMedico dados) {
         this.nome = dados.nome();
         this.email = dados.email();
+        this.telefone = dados.telefone();
         this.crm = dados.crm();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
