3354071905ef5010f622bf901992b980b6ac76ff
cortezinho <gabrielcortez2802@gmail.com>
2025-11-07 20:56:43 -0300

    medpro

--- AWEB/medpro/src/main/java/com/medpro/medpro/controller/MedicoController.java
+++ AWEB/medpro/src/main/java/com/medpro/medpro/controller/MedicoController.java
@@ -1,16 +1,23 @@
 package com.medpro.medpro.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.medpro.medpro.model.dto.DadosCadastroMedico;
+import com.medpro.medpro.model.dto.DadosListagemMedico;
 import com.medpro.medpro.model.entity.Medico;
 import com.medpro.medpro.repository.MedicoRepository;
 
 import jakarta.transaction.Transactional;
+
 
 @RestController
 @RequestMapping("medicos")
@@ -25,4 +32,9 @@ public class MedicoController {
         medicoRepository.save(new Medico(dados));
     }
 
+    @GetMapping
+    public Page<DadosListagemMedico> listar(Pageable paginacao) {
+        return medicoRepository.findAll(paginacao).map(DadosListagemMedico::new);
+    }
+    
 }
